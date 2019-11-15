224fd5414e2f3572ea645fa8daa65a8d84d1ce8a
gmocore <gmocore@gmail.com>
2019-11-15 11:42:54 -0700

    debugging click count

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
 import "./App.css";
 import Nav from "./components/Nav/Nav";
-import Footer from './components/Footer/Footer'
+import Footer from "./components/Footer/Footer";
 import characters from "./characters.json";
 import Photo from "./components/Photo/Photo";
-import Message from './components/Message'
-
+import Message from "./components/Message";
 
 class App extends Component {
   state = {
@@ -13,7 +12,8 @@ class App extends Component {
     score: 0,
     clicked: [],
     highScore: 0,
-    message: "Click an image to begin, don't click the same image twice or they get clipped!"
+    message:
+      "Click an image to begin, don't click the same image twice or they get clipped!"
   };
 
   renderCards = () =>
@@ -23,23 +23,38 @@ class App extends Component {
         alt={name}
         key={id}
         id={id}
-        // onClick={()=> this.shuffle(this.state.characters)}
         onClick={this.cardClicked}
       />
     ));
 
   updateScore = () => {
-    this.setState({ score: this.state.score + 1, message: 'Correct!' });
-    setTimeout(() => {
-      this.setState({message: `Don't screw up!`})
-      }, 750);
+    debugger
+    this.setState({score: this.state.score + 1});
+    // if (this.state.score > 1) {
+    //   this.setState({
+    //     message: "You Win! looks like you know all about them corner boys",
+    //     score: 0,
+    //     clicked: []
+    //   });
+    // } else {
+    //   this.setState({ score: this.state.score + 1, message: "Correct!" });
+    //   setTimeout(() => {
+    //     this.setState({ message: `Don't screw up!` });
+    //   }, 750);
+    // }
   };
 
+
+
   gameOver = () => {
-    this.setState({ score: 0, clicked: [], message: 'you already guessed that, you just dropped a body' });
-     document.querySelector('.message').classList.add('text-danger')
-     document.querySelector('.images').classList.add('shake')
-  } 
+    this.setState({
+      score: 0,
+      clicked: [],
+      message: "you already guessed that, you just dropped a body, click a card to try again"
+    });
+    document.querySelector(".message").classList.add("text-danger");
+    document.querySelector(".images").classList.add("shake");
+  };
 
   checkHighScore = () => {
     if (this.state.score >= this.state.highScore) {
@@ -48,15 +63,14 @@ class App extends Component {
   };
 
   cardClicked = e => {
-    document.querySelector('.message').classList.remove('text-danger');
-    document.querySelector('.images').classList.remove('shake')
-    
+    document.querySelector(".message").classList.remove("text-danger");
+    document.querySelector(".images").classList.remove("shake");
+
     let id = e.target.id;
     this.setState({ clicked: [...this.state.clicked, id] });
     this.shuffle(this.state.characters);
     if (this.state.clicked.includes(id)) {
       this.gameOver();
-      
     } else {
       this.updateScore();
       this.checkHighScore();
@@ -80,12 +94,8 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <Nav
-          score={this.state.score}
-          highScore={this.state.highScore}
-          
-        />
-        <Message message={this.state.message}/>
+        <Nav score={this.state.score} highScore={this.state.highScore} />
+        <Message message={this.state.message} />
         <div className="container images mb-3 p-2">{this.renderCards()}</div>
         <Footer />
       </div>
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,10 +3,12 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="bg-dark sticky-bottom p-3">
-      <h3 className="text-center text-light">
-        Based on the critically acclaimed HBO series The Wire 
-      </h3>
-      
+      <p className="text-center text-light">
+        Based on the critically acclaimed HBO series
+        <span>
+          <h4 className="">The Wire</h4>
+        </span>
+      </p>
     </div>
   );
 };
