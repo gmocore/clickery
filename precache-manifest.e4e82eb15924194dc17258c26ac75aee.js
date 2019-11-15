bed1dcc953963560a89bf0cfb15e83c0d6f40f0c
gmocore <gmocore@gmail.com>
2019-11-15 13:37:27 -0700

    Updates

---
+++ precache-manifest.e4e82eb15924194dc17258c26ac75aee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57ef3b22105570fdd2758350b41e2b11",
+    "url": "/clickery/index.html"
+  },
+  {
+    "revision": "392ecbe34035af3eab5c",
+    "url": "/clickery/static/css/main.ac603169.chunk.css"
+  },
+  {
+    "revision": "f93346ab5d35c21ff98b",
+    "url": "/clickery/static/js/2.3bc679f7.chunk.js"
+  },
+  {
+    "revision": "392ecbe34035af3eab5c",
+    "url": "/clickery/static/js/main.d38d0347.chunk.js"
+  },
+  {
+    "revision": "c5fadb6c2f100c95c447",
+    "url": "/clickery/static/js/runtime-main.43b32d55.js"
+  }
+]);
